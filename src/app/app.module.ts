368dba7f2b32552fd95eb7880db8b7d1567c984d
Luis <estrada.villegas.17071@itsmante.edu.mx>
2021-01-27 15:31:29 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts';
+
+
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarrasComponent } from './components/barras/barras.component';
+import { DonasComponent } from './components/donas/donas.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarrasComponent,
+    DonasComponent,
+    RadarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
